253602b782d82a4b81e074e9389fc7ae21e5e069
AnceSkalze <ance.skalze@grobinasskola.lv>
2021-10-31 14:14:38 +0200

    Treniņš1

--- darbs.js
+++ darbs.js
@@ -1,8 +1,19 @@
-setTimeout("document.bgColor = 'Azure'", 2000); 
+/*
+setTimeout("document.bgColor = 'Azure'", 2000); // komentārs 
 setTimeout("document.bgColor = 'LightCyan'", 4000);
 setTimeout("document.bgColor = 'AquaMarine'", 6000);
-setTimeout("document.bgColor = 'Aqua'", 8000);
+
+//setTimeout("document.bgColor = 'Aqua'", 8000);
 setTimeout("document.bgColor = 'Cyan'", 10000);
 setTimeout("document.bgColor = 'DarkCyan'", 12000);
 setTimeout("document.bgColor = 'Teal'", 14000);
 setTimeout("document.bgColor = 'DarkSlateGrey'", 16000);
+*/
+setTimeout("document.bgColor = 'DarkSlateGrey'" , 16000);
+
+//alert(3+"9"+6); Paziņojuma lodziņš
+//console.log(5+8+"15");
+function aka(){
+    document.getElementById("paka").innerHTML=4+9;
+}
+document.write("astīte")
